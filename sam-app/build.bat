0f0dbac6d142f90160a653bb24aa8dc1eef39a3a
Jorge Orozco <yorchorozco@gmail.com>
2019-09-28 18:43:29 +0200

    adjusting repo

--- sam-app/build.bat
+++
@@ -1,6 +0,0 @@
-echo off
-
-sam build
-sam package --output-template packaged.yaml --s3-bucket objdetection-orozcobusch
-sam deploy --template-file packaged.yaml --region eu-central-1 --capabilities CAPABILITY_IAM --stack-name aws-sam-getting-started
-aws cloudformation describe-stacks --stack-name aws-sam-getting-started --region eu-central-1 --query "Stacks[].Outputs"
